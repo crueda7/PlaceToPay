a611b6d6c3d6c1f429d438ad532bd7d8a7e87b36
Carlos Rueda <cjruedamena@gmail.com>
2022-10-02 15:50:24 -0500

    Add feature order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,84 +2,114 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\InformationRequest;
 use App\Models\Order;
+use App\Models\OrderDetail;
+use App\Models\ShoppingCart;
+use App\ResponseHelper;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class OrderController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
-        //
+        $orders = Order::with(['orderDetails.product'])
+            ->where('orders.user_id', '=', Auth::user()->id)
+            ->orderBy('created_at', 'DESC')->get();
+
+        return Inertia::render('Orders', [
+            'orders' => $orders,
+        ]);
+    }
+
+    /**
+     * Example queue job for pending orders.
+     *
+     * @return void
+     */
+    public function sync()
+    {
+        $failedOrders = Order::where('status', '=', 'PENDING')->get();
+        foreach ($failedOrders as $order)
+            $this->dispatch(new InformationRequest($order));
+
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
-        //
+        $user = Auth::user();
+        $order = new Order;
+        $order['customer_name'] = $user['name'];
+        $order['customer_email'] = $user['email'];
+        $order['user_id'] = $user['id'];
+        $cart = DB::table('shopping_carts')
+            ->join('products', 'shopping_carts.product_id', '=', 'products.id')
+            ->select('products.*', 'shopping_carts.id AS cart_id')
+            ->where('shopping_carts.user_id', '=', $user->id)
+            ->get();
+
+        return Inertia::render('Order/Order', [
+            'order' => $order,
+            'cart' => $cart,
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return false|string
+     * @throws ValidationException
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'customer_name' => 'required|string',
+            'customer_email' => 'required|string',
+            'customer_mobile' => 'required|string',
+            'status' => 'required|string',
+            'user_id' => 'required|integer',
+            'cart' => 'required|array',
+        ]);
+
+        if ($validator->fails()) {
+            return json_encode(ResponseHelper::response('2',$validator->errors()->first(),[]));
+        }
+
+        $validated = $validator->validated();
+
+        $order = $request->user()->orders()->create($validated);
+
+        collect($request->only(['cart']))
+            ->map(function ($array) use ($order) {
+                foreach ($array as $item) {
+                    $orderDetail = new OrderDetail([
+                        'quantity' => 1,
+                        'product_id' => $item['id'],
+                        'order_id' => $order->id,
+                    ]);
+                    $orderDetail->save();
+                    ShoppingCart::find($item['cart_id'])->delete();
+                }
+            });
+
+        return redirect(route('orders.checkout', [$order, 'placetopay']));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Order  $order
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Order $order)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Order  $order
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Order $order)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Order  $order
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Order $order)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Order  $order
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Order $order)
-    {
-        //
-    }
 }
---
+++ resources/js/Pages/Order/Order.vue
@@ -0,0 +1,138 @@
+<script setup>
+import {Head, useForm} from '@inertiajs/inertia-vue3';
+import {computed, onMounted} from "vue";
+import {useStore} from "vuex";
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import TextInput from '@/Components/TextInput.vue';
+import InputError from '@/Components/InputError.vue';
+import CardProductDetail from "@/Components/CardProductDetail.vue";
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+
+const store = useStore();
+
+const props = defineProps({
+    order: Object,
+    cart: Object,
+});
+
+const total = computed(() => {
+    let total = 0;
+
+    props.cart.forEach(item => {
+        total += item.price;
+    })
+
+    return total;
+});
+
+const form = useForm({
+    customer_name: '',
+    customer_email: '',
+    customer_mobile: '',
+    status: '',
+    user_id: '',
+    cart: [],
+});
+
+onMounted(() => {
+    console.log(props.order);
+    console.log(props.cart);
+
+    if (Object.keys(props.cart).length === 0) {
+        back();
+    }
+});
+
+async function sendAxios() {
+    let order = props.order;
+
+    store.commit('setLoading', true);
+
+    await axios.post('/storeOrders', {
+        customer_name: order.customer_name,
+        customer_email: order.customer_email,
+        customer_mobile: form.customer_mobile,
+        status: 'PENDING',
+        user_id: order.user_id,
+        cart: props.cart,
+    }).then((response) => {
+        store.commit('setLoading', false);
+        useForm().get(route('orders.index'));
+        window.open(response.data.processUrl, '_blank');
+    }).catch((error) => {
+        store.commit('setLoading', false);
+
+        Toast.fire({
+            icon: 'error',
+            title: error,
+        });
+    });
+};
+
+function back() {
+    useForm().get(route('products.index'));
+};
+</script>
+
+<template>
+    <Head title="Order" />
+
+    <AuthenticatedLayout>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white dark:bg-gray-600">
+                        <h2 class="py-2 text-3xl font-bold tracking-tight text-orange-500">Information
+                            order 🤑</h2>
+
+                        <div class="grid grid-cols-1 gap-8 p-6 bg-white dark:bg-gray-800 rounded border-gray-200">
+                            <form class="p-6 dark:bg-gray-600 rounded" @submit.prevent="sendAxios()">
+                                <div>
+                                    <InputLabel class="dark:text-gray-800" for="name" value="Name"/>
+                                    <TextInput id="name" type="text" class="mt-1 block w-full" v-model="form.customer_name" :model-value="order.customer_name" required autofocus autocomplete="name"/>
+                                    <InputError class="mt-2" :message="form.errors.customer_name"/>
+                                </div>
+
+                                <div class="mt-4">
+                                    <InputLabel class="dark:text-gray-800" for="email" value="Email"/>
+                                    <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.customer_email" :model-value="order.customer_email" required autocomplete="username"/>
+                                    <InputError class="mt-2" :message="form.errors.customer_email"/>
+                                </div>
+
+                                <div class="mt-4">
+                                    <InputLabel class="dark:text-gray-800" for="mobile" value="Mobile"/>
+                                    <TextInput id="mobile" type="text" class="mt-1 block w-full" v-model="form.customer_mobile" required autofocus autocomplete="name"/>
+                                    <InputError class="mt-2" :message="form.errors.customer_mobile"/>
+                                </div>
+
+                                <h2 class="pt-6 text-2xl font-bold tracking-tight text-gray-500 dark:text-white">Detail order 🛒</h2>
+
+                                <div class="grid grid-cols-1 gap-8 p-6 bg-white dark:bg-gray-600 rounded border-gray-200">
+                                    <template v-for="(product, index) in cart" :key="'product'+index">
+                                        <CardProductDetail :product="product" :showButton="false"></CardProductDetail>
+                                    </template>
+
+                                    <div class="py-2 flex justify-end items-center">
+                                        <span class="text-3xl font-bold text-gray-900 dark:text-white">
+                                            Total:
+                                            <span class="text-3xl font-bold text-orange-500" v-html="'$'+total"></span>
+                                        </span>
+                                    </div>
+
+                                    <div class="py-2 flex justify-end items-center">
+                                        <PrimaryButton class="ml-4 h-14">Pay ❤</PrimaryButton>
+                                    </div>
+                                </div>
+
+                                <div class="grid grid-cols-1 gap-8 p-6">
+
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
